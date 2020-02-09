207aed6b8ab0960b6d6a4920f0e846ad8f4848fa
Moisés Osti <moises.osti@gmail.com>
2020-02-09 18:50:10 -0300

    Observable parte 1

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
+import { Observer } from 'rxjs/Observer';
+import { Subscription } from 'rxjs/Subscription';
 
 import { OfertasService } from './../ofertas.service';
 import { Oferta } from './../shared/oferta.model';
@@ -10,7 +14,10 @@ import { Oferta } from './../shared/oferta.model';
   styleUrls: ['./oferta.component.scss'],
   providers: [ OfertasService ]
 })
-export class OfertaComponent implements OnInit {
+export class OfertaComponent implements OnInit, OnDestroy {
+  
+  // private tempoObservableSubscription: Subscription
+  // private meuObservableTesteSubscription: Subscription
 
   public id: number
   public oferta: Oferta
@@ -33,11 +40,50 @@ export class OfertaComponent implements OnInit {
     // Recuperando parâmetros da rota com Snapshot
     // console.log( this.route.snapshot.params.id );
 
+    /*
     // Recuperando parâmetros da rota com Subscribe
-    this.route.params.subscribe((parametro: any) => {
-      // console.log(parametro.id);
-    });
+    this.route.params.subscribe(
+      (parametro: any) => { console.log( parametro ) },
+      (erro: any) => { console.log(erro) },
+      () => { console.log("Processamento foi classificado como concluído!") }
+      )
+    */
 
+    /*
+    // Utilização do método Interval
+    let tempo = Observable.interval(2000)
+
+    this.tempoObservableSubscription = tempo.subscribe((intervalo: number) => {
+      console.log(intervalo)
+    })
+    */
+
+    // Observable (observável)
+    /*
+    let meuObservableTeste = Observable.create((observer: Observer<number>) => {
+      // observer.next('Primeiro evendo da stream')
+      // observer.next('Segundo evendo da stream')
+      observer.next(1)
+      observer.next(2)
+      // observer.error('Algum erro foi encontrado na stream de eventos')
+      observer.complete()
+      observer.next(3)
+    })
+    */
+
+    // Observable (Observador)
+    /*
+    this.meuObservableTesteSubscription = meuObservableTeste.subscribe(
+      (resultado: number) => console.log(resultado * 10),
+      (erro: string) => console.log(erro),
+      () => console.log('Stream de eventos foi finalizada')
+    )
+    */
+  }
+
+  ngOnDestroy() {
+    // this.meuObservableTesteSubscription.unsubscribe()
+    // this.tempoObservableSubscription.unsubscribe()
   }
 
 }
